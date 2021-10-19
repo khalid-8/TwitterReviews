c98f6b0802e140a7b83c415e7080041bda75aece
khalid-8 <kalnahdi1@gmail.com>
2021-10-19 17:00:37 +1100

    * Updated the CSS styling * Created Slilding devs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Header"> <img className="Logo" src="images/Logo.png" alt="Twitter Reviews's Logo"/> </div>
+      <div className="Header"> <img id='twit_revLogo' className="Logo" src="images/Logo.png" alt="Twitter Reviews's Logo"/> </div>
       <div className="Entry">
-        <b className="Title">Twitter Reviews</b>
+        <h1 className="Title">Twitter Reviews</h1>
         <input className="SearchBar" type="text" placeholder="Search For a product or services" />
-      </div>
+        </div>
+        <div className='Appear'>
+          <b>1000 Tweets</b>
+        </div>
     </div>
   );
 }
