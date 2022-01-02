0183b18a3bb1599dad6b1d5f9ce5042e20522562
khalid-8 <62469292+khalid-8@users.noreply.github.com>
2022-01-02 09:57:05 +0300

    Update api.js

--- src/helprs/api.js
+++ src/helprs/api.js
@@ -30,32 +30,6 @@ class API {
     // console.log(res.data);
     // setIP(res.data.IPv4)
   }
-  //Create a new content to the DB
-  addContent = async (objToAdd) => {
-    return await axiosInstance.post("insertContent", objToAdd).then((response) => {
-      return response.data;
-    }).catch((err) => {
-      return err;
-    });
-  };
-
-  //Delete a content from the DB
-  deleteContent = async (objToDelete) => {
-    return await axiosInstance.delete("deleteContent", {data: {objToDelete}}).then((response) => {
-      return response.data;
-    }).catch((err) => {
-      return err;
-    });
-  };
-
-   //Edit a content in the DB
-    editContent = async (objToEdit) => {
-    return await axiosInstance.put("editContent", objToEdit).then((response) => {
-      return response.data;
-    }).catch((err) => {
-      return err;
-    });
-  };
 
 }
 
